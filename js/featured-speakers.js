bd71d43e96e374bd22450db5db247f4782f859aa
AmineHLub <Amino.bhl@gmail.com>
2021-09-07 13:37:30 +0100

    Add footer and style
    Add partner list
    
    Fix src pictures location in js array

--- js/featured-speakers.js
+++ js/featured-speakers.js
@@ -1,36 +1,36 @@
 const arrOfSpeakers = [
   {
-    picture: '../src/page2/speaker-faces/face1.png',
+    picture: './src/page2/speaker-faces/face1.png',
     name: 'Bill',
     position: 'CEO at WEBuild',
     description: 'Ex-professor at Harvard CS50, Created WEBuild in 2009 to help people get opportunity allover the world',
   },
   {
-    picture: '../src/page2/speaker-faces/face2.png',
+    picture: './src/page2/speaker-faces/face2.png',
     name: 'Max',
     position: 'Senior Professor at Harvard',
     description: 'Teached in all methodological ways for over 20 years',
   },
   {
-    picture: '../src/page2/speaker-faces/face3.png',
+    picture: './src/page2/speaker-faces/face3.png',
     name: 'Adam',
     position: 'Career Coach at themuse',
     description: 'Helped over 10,000 people take the right decisions and get the career they wanted',
   },
   {
-    picture: '../src/page2/speaker-faces/face4.png',
+    picture: './src/page2/speaker-faces/face4.png',
     name: 'Selena',
     position: 'Teacher at university of british columbia',
     description: '15 years of experience and going, and major contributer to curriculum managing and planning at WEBuild',
   },
   {
-    picture: '../src/page2/speaker-faces/face5.png',
+    picture: './src/page2/speaker-faces/face5.png',
     name: 'Martha',
     position: 'Chef of CS department at Indian Institute of Technology Delhi',
     description: 'Phd in Computer Science, has over 20 books on Algorithms and Data Science',
   },
   {
-    picture: '../src/page2/speaker-faces/face6.png',
+    picture: './src/page2/speaker-faces/face6.png',
     name: 'Fatma',
     position: 'Curriculum manager',
     description: 'A top graduate from University of liverpool, with 10 years of curriculum managing experience',
