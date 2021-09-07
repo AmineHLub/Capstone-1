bc911efedfc44991fb3d46e595e37dd7c92a8231
AmineHLub <Amino.bhl@gmail.com>
2021-09-07 14:01:30 +0100

    fix program pictures

--- js/programs.js
+++ js/programs.js
@@ -1,36 +1,36 @@
 const arrOfPrograms = [
   {
-    logo: '../src/page2/program-logos/html.svg',
+    logo: './src/page2/program-logos/html.svg',
     title: 'HTML',
     description: 'HTML is the standard markup language for Web pages. With HTML you can create your own Website. HTML is easy to learn - You will enjoy it!',
   },
   {
-    logo: '../src/page2/program-logos/css.svg',
+    logo: './src/page2/program-logos/css.svg',
     title: 'CSS',
     description: 'CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.',
   },
   {
-    logo: '../src/page2/program-logos/js.svg',
+    logo: './src/page2/program-logos/js.svg',
     title: 'JavaScript',
     description: 'JavaScript is the world\'s most popular programming language. JavaScript is the programming language of the Web. JavaScript is easy to learn.',
   },
   {
-    logo: '../src/page2/program-logos/ruby.svg',
+    logo: './src/page2/program-logos/ruby.svg',
     title: 'Ruby',
     description: 'Ruby is an interpreted, high-level, general-purpose programming language. It supports multiple programming paradigms, and functional programming.',
   },
   {
-    logo: '../src/page2/program-logos/ror.svg',
+    logo: './src/page2/program-logos/ror.svg',
     title: 'RoR',
     description: 'server-side web application framework written in Ruby under the MIT License.',
   },
   {
-    logo: '../src/page2/program-logos/react.svg',
+    logo: './src/page2/program-logos/react.svg',
     title: 'React',
     description: 'A JavaScript library for building user interfaces. React makes it painless to create interactive UIs.',
   },
   {
-    logo: '../src/page2/program-logos/github.svg',
+    logo: './src/page2/program-logos/github.svg',
     title: 'Github',
     description: 'GitHub is where over 65 million developers shape the future of software, together.',
   },
