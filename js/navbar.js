4eb592c734eeffc603579352a4051de03959eae3
AmineHLub <Amino.bhl@gmail.com>
2021-09-08 06:06:37 +0100

    Add all icons to mobile menu
    Add go up button (effectless tho)
    
    Add overflow going hiddeen when mobile menu is opened

--- js/navbar.js
+++ js/navbar.js
@@ -4,11 +4,13 @@ const closeMenu = document.querySelector('.hamburger-menu-clicked > a');
 function show() {
   document.querySelector('.hamburger-menu-clicked').classList.remove('close');
   document.querySelector('.hamburger-menu-clicked').classList.add('show');
+  document.querySelector('*').style.overflow = 'hidden';
 }
 
 function close() {
   document.querySelector('.hamburger-menu-clicked').classList.remove('show');
   document.querySelector('.hamburger-menu-clicked').classList.add('close');
+  document.querySelector('*').style.overflow = 'auto';
 }
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
