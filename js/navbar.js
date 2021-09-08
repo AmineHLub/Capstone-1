77a5939ddb20c6afc63d1245b29cc307caea5b48
AmineHLub <Amino.bhl@gmail.com>
2021-09-08 05:08:46 +0100

    Finish Hamburger-menu styling
    add effectless scrollup button

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ const openMenu = document.querySelector('.hamburger-menu');
 const closeMenu = document.querySelector('.hamburger-menu-clicked > a');
 
 function show() {
-document.querySelector('.hamburger-menu-clicked').classList.remove('close');
+  document.querySelector('.hamburger-menu-clicked').classList.remove('close');
   document.querySelector('.hamburger-menu-clicked').classList.add('show');
 }
 
@@ -12,3 +12,4 @@ function close() {
 }
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
+document.querySelector('.menu-list > ul').addEventListener('click', close);
